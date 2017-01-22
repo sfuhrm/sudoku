3c2237a9d297bc3651c4e67ff941f48c44beec83
Stephan Fuhrmann <s_fuhrm@gmx.de>
2017-01-22 16:46:17 +0100

    Removed import

--- src/test/java/de/sfuhrm/sudoku/CreatorTest.java
+++ src/test/java/de/sfuhrm/sudoku/CreatorTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Ignore;
