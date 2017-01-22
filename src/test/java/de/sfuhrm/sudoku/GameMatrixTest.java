4c9d1698c04480fd651c54dacf3b63d5adeb6aa5
Stephan Fuhrmann <s_fuhrm@gmx.de>
2017-01-22 16:55:57 +0100

    testParse()

--- src/test/java/de/sfuhrm/sudoku/GameMatrixTest.java
+++ src/test/java/de/sfuhrm/sudoku/GameMatrixTest.java
@@ -34,4 +34,50 @@ public class GameMatrixTest {
         value = matrix.get(0, 0);
         assertEquals(4, value);
     }
+    
+    @Test
+    public void testParse() {
+        byte data[][] =
+        GameMatrix.parse(
+                "000000000",
+                "111111111",
+                "222222222",
+                "333333333",
+                "444444444",
+                "555555555",
+                "666666666",
+                "777777777",
+                "888888888"
+                );
+        
+        assertEquals(9, data[0].length);
+        assertEquals(9, data[1].length);
+        assertEquals(9, data[2].length);
+        assertEquals(9, data[3].length);
+        assertEquals(9, data[4].length);
+        assertEquals(9, data[5].length);
+        assertEquals(9, data[6].length);
+        assertEquals(9, data[7].length);
+        assertEquals(9, data[8].length);
+        
+        assertEquals(0, data[0][0]);
+        assertEquals(0, data[0][1]);
+        assertEquals(1, data[1][0]);
+        assertEquals(1, data[1][1]);
+        assertEquals(2, data[2][0]);
+        assertEquals(2, data[2][1]);
+        assertEquals(3, data[3][0]);
+        assertEquals(3, data[3][1]);
+        assertEquals(4, data[4][0]);
+        assertEquals(4, data[4][1]);
+        assertEquals(5, data[5][0]);
+        assertEquals(5, data[5][1]);
+        assertEquals(6, data[6][0]);
+        assertEquals(6, data[6][1]);
+        assertEquals(7, data[7][0]);
+        assertEquals(7, data[7][1]);
+        assertEquals(8, data[8][0]);
+        assertEquals(8, data[8][1]);
+    }
+    
 }
