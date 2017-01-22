42fa30d1ac2c4f5cd1fd286d4894f221228460a8
Stephan Fuhrmann <s_fuhrm@gmx.de>
2017-01-22 17:18:22 +0100

    Common unit test utilities

---
+++ src/test/java/de/sfuhrm/sudoku/Utility.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.sfuhrm.sudoku;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Utility class.
+ */
+public class Utility {
+
+    private Utility() {
+        // no instance allowed
+    }
+    
+    public static List<Integer> toIntList(byte[] array) {
+        int[] iv = toIntArray(array);
+        return IntStream.of(iv).mapToObj((int b) -> b).sorted().collect(Collectors.toList());
+    }
+
+    public static int[] toIntArray(byte[] array) {
+        int[] iv = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            iv[i] = array[i];
+        }
+        return iv;
+    }
+}
