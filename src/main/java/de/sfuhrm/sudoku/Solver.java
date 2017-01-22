f090ca6c32deb00ad10396537745a5e4b963c669
Stephan Fuhrmann <s_fuhrm@gmx.de>
2017-01-22 20:27:32 +0100

    Removed main method Some refactoring Renaming of fields to more suitable names

--- src/main/java/de/sfuhrm/sudoku/Solver.java
+++ src/main/java/de/sfuhrm/sudoku/Solver.java
@@ -19,14 +19,9 @@ Boston, MA  02110-1301, USA.
 */
 package de.sfuhrm.sudoku;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Vector;
 
 /**
  * Solves a partially filled Sudoku. Can find multiple solutions if they are
@@ -38,45 +33,43 @@ public class Solver {
     /**
      * Current working copy.
      */
-    private Riddle field;
+    private final Riddle field;
+    
+    /** The possible solutions for this riddle. */
+    private List<Riddle> possibleSolutions;
 
-    private List<Riddle> solvedFields;
+    /** The maximum number of solutions to search. */
+    public final static int LIMIT = 20;
 
-    public final static int limit = 20;
-
-    Solver(Riddle f) {
+    public Solver(Riddle f) {
         field = (Riddle) f.clone();
-        solvedFields = new ArrayList<>();
+        possibleSolutions = new ArrayList<>();
     }
 
     /**
      * Solves the Sudoku problem.
-     *
      * @return the found solutions. Should be only one.
      */
     public List<Riddle> solve() {
-
-        solvedFields.clear();
+        possibleSolutions.clear();
         int freeCells = GameMatrix.SIZE * GameMatrix.SIZE - field.getSetCount();
 
         backtrack(freeCells);
 
-        return Collections.unmodifiableList(solvedFields);
+        return Collections.unmodifiableList(possibleSolutions);
     }
 
     /**
-     * Solves a sudoku.
-     *
-     * @param freeCells number of free cells, abort criterion
-     * @return the number of solutions.
+     * Solves a Sudoku using backtracking.
+     * @param freeCells number of free cells, abort criterion.
+     * @return the total number of solutions.
      */
     private int backtrack(int freeCells) {
 
         // just one result, we have no more to choose
         if (freeCells == 0) {
-
-            if (solvedFields.size() < limit) {
-                solvedFields.add((Riddle) field.clone());
+            if (possibleSolutions.size() < LIMIT) {
+                possibleSolutions.add((Riddle) field.clone());
             }
 
             return 1;
@@ -138,42 +131,5 @@ public class Solver {
             result = 0;
         }
         return result;
-    }
-
-    public static void main(String args[]) throws IOException {
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
-
-        Vector<String> lines = new Vector<String>();
-
-        for (int i = 0; i < Riddle.SIZE; i++) {
-            String line = br.readLine();
-            lines.add(line);
-        }
-
-        byte data[][] = new byte[Riddle.SIZE][Riddle.SIZE];
-
-        for (int i = 0; i < Riddle.SIZE; i++) {
-            for (int j = 0; j < Riddle.SIZE; j++) {
-                String line = lines.get(i);
-                char digit = line.length() > j ? line.charAt(j) : '0';
-                int val = Integer.valueOf(digit + "");
-                data[j][i] = (byte) val;
-            }
-        }
-
-        Riddle f = new Riddle();
-        f.setAll(data);
-        System.out.println(f);
-
-        Solver s = new Solver(f);
-        Collection<Riddle> results = s.solve();
-
-        int i = 0;
-        for (Riddle fd : results) {
-            i++;
-            System.out.println(i + ".");
-            System.out.println(fd);
-        }
     }
 }
