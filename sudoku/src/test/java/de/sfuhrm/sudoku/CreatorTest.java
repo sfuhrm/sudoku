1298f749982153f4990fc733e4f17e5775df937a
Stephan Fuhrmann <s@sfuhrm.de>
2018-04-19 00:06:25 +0200

    More tests

--- sudoku/src/test/java/de/sfuhrm/sudoku/CreatorTest.java
+++ sudoku/src/test/java/de/sfuhrm/sudoku/CreatorTest.java
@@ -22,8 +22,8 @@ package de.sfuhrm.sudoku;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Test for {@link Creator}.
@@ -35,29 +35,53 @@ public class CreatorTest {
         int v = Creator.getSetBitOffset(0, 0);
         assertEquals(-1, v);
     }
-    
+
     @Test
     public void testGetSetBitOffsetWithNothingSet2() {
         int v = Creator.getSetBitOffset(0, 1);
         assertEquals(-1, v);
     }
-    
+
     @Test
     public void testGetSetBitOffsetWithTwoSet1() {
         int v = Creator.getSetBitOffset(3, 0);
         assertEquals(0, v);
-    }    
-    
+    }
+
     @Test
     public void testGetSetBitOffsetWithTwoSet2() {
         int v = Creator.getSetBitOffset(3, 1);
         assertEquals(1, v);
     }
-    
+
+    @Test
+    public void testGetSetBitOffsetWithTreeSet1() {
+        int v = Creator.getSetBitOffset(0x111, 0);
+        assertEquals(0, v);
+    }
+
+    @Test
+    public void testGetSetBitOffsetWithTreeSet2() {
+        int v = Creator.getSetBitOffset(0x111, 1);
+        assertEquals(4, v);
+    }
+
+    @Test
+    public void testGetSetBitOffsetWithTreeSet3() {
+        int v = Creator.getSetBitOffset(0x111, 2);
+        assertEquals(8, v);
+    }
+
+    @Test
+    public void testGetSetBitOffsetWithTreeSet4() {
+        int v = Creator.getSetBitOffset(0x111, 3);
+        assertEquals(-1, v);
+    }
+
     @Test
     public void testGetSetBitOffsetWithAll() {
         int v;
-        
+
         v = Creator.getSetBitOffset(0xffffffff, 0);
         assertEquals(0, v);
         v = Creator.getSetBitOffset(0xffffffff, 1);
@@ -77,15 +101,15 @@ public class CreatorTest {
         v = Creator.getSetBitOffset(0xffffffff, 8);
         assertEquals(8, v);
     }
-    
-    
+
+
     @Test
     public void testCreateFull() {
         GameMatrix r = Creator.createFull();
         assertEquals(9*9, r.getSetCount());
         assertEquals(true, r.isValid());
     }
-    
+
     @Test
     public void testCreateFullWithMultipleInvocations() {
 //        for (int i=0; i < 100000; i++) {
@@ -95,10 +119,10 @@ public class CreatorTest {
             assertEquals(true, r.isValid());
         }
     }
-    
+
     @Test
     public void testCreateRiddle() {
-        GameMatrix matrix = Creator.createFull();        
+        GameMatrix matrix = Creator.createFull();
         Riddle riddle = Creator.createRiddle(matrix);
         for (int i=0; i < GameMatrix.SIZE; i++) {
             for (int j=0; j < GameMatrix.SIZE; j++) {
@@ -117,27 +141,27 @@ public class CreatorTest {
     public void testCreateNumbersToDistributeWithOnce() {
         byte v[] = Creator.createNumbersToDistribute(new Random(), 1);
         List<Integer> intList = Utility.toIntList(v);
-        
+
         assertEquals(9, intList.size());
         assertEquals(Arrays.asList(1,2,3,4,5,6,7,8,9), intList);
     }
-    
+
     @Test
     public void testCreateNumbersToDistributeWithTwice() {
         byte v[] = Creator.createNumbersToDistribute(new Random(), 2);
         List<Integer> intList = Utility.toIntList(v);
-        
+
         assertEquals(2*9, intList.size());
         assertEquals(Arrays.asList(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9), intList);
     }
-    
+
     @Test
     public void testCreateVariant() {
         GameMatrix original = Creator.createFull();
         GameMatrix variant1 = Creator.createVariant(original);
         GameMatrix variant2 = Creator.createVariant(original);
         GameMatrix variant3 = Creator.createVariant(original);
-        
+
         assertEquals(true, original.isValid());
         assertEquals(9*9, original.getSetCount());
         assertEquals(true, variant1.isValid());
@@ -146,8 +170,8 @@ public class CreatorTest {
         assertEquals(9*9, variant2.getSetCount());
         assertEquals(true, variant3.isValid());
         assertEquals(9*9, variant3.getSetCount());
-    }    
-    
+    }
+
     @Test
     public void testSwapColumnWithFirstAndSecond() {
         GameMatrix actual = new GameMatrix();
@@ -175,9 +199,9 @@ public class CreatorTest {
                 "000001000",
                 "000000001"
                 ));
-                
+
         Creator.swapColumn(actual, 0, 1);
-        
+
         assertEquals(expected, actual);
      }
 
@@ -208,12 +232,12 @@ public class CreatorTest {
                 "000001000",
                 "100000000"
                 ));
-                
+
         Creator.swapColumn(actual, 0, 8);
-        
+
         assertEquals(expected, actual);
      }
-    
+
     @Test
     public void testSwapRowWithFirstAndSecond() {
         GameMatrix actual = new GameMatrix();
@@ -241,12 +265,12 @@ public class CreatorTest {
                 "000001000",
                 "000000001"
                 ));
-                
+
         Creator.swapRow(actual, 0, 1);
-        
+
         assertEquals(expected, actual);
      }
-    
+
     @Test
     public void testSwapRowWithFirstAndLast() {
         GameMatrix actual = new GameMatrix();
@@ -274,9 +298,9 @@ public class CreatorTest {
                 "000001000",
                 "100000000"
                 ));
-                
+
         Creator.swapRow(actual, 0, 8);
-        
+
         assertEquals(expected, actual);
      }
 
