b9a7b1e07efc15e18d871d59ce64d49f81f020f6
Stephan Fuhrmann <s@sfuhrm.de>
2018-04-22 11:22:31 +0200

    Fix bool expr

--- sudoku/src/main/java/de/sfuhrm/sudoku/GameMatrix.java
+++ sudoku/src/main/java/de/sfuhrm/sudoku/GameMatrix.java
@@ -204,9 +204,9 @@ public class GameMatrix implements Cloneable {
      * @param value the value of the field.
      */
     public void set(final int row, final int column, final byte value) {
-        assert value >= MINIMUM_VALUE
-                && value <= MAXIMUM_VALUE
-                && value != UNSET
+        assert (value >= MINIMUM_VALUE
+                && value <= MAXIMUM_VALUE)
+                || value == UNSET
                 : "Value out of range: " + value;
         data[row][column] = value;
     }
