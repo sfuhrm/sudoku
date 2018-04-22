433788addae1e68aa9c787d202f7f8d15b6c1dfa
Stephan Fuhrmann <s@sfuhrm.de>
2018-04-22 13:46:58 +0200

    Test solver with real example

--- sudoku/src/test/java/de/sfuhrm/sudoku/SolverTest.java
+++ sudoku/src/test/java/de/sfuhrm/sudoku/SolverTest.java
@@ -21,46 +21,80 @@ package de.sfuhrm.sudoku;
 
 import java.util.List;
 import java.util.Random;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Test for {@link Solver}.
  * @author Stephan Fuhrmann
  */
 public class SolverTest {
-    
+
     @Test
     public void testSolveWithNoStep() {
         // create full matrix
         GameMatrix matrix = Creator.createFull();
-        
+
         Riddle riddle = new Riddle();
         riddle.setAll(matrix.getArray());
-        
+
         Solver solver = new Solver(riddle);
         List<Riddle> solutions = solver.solve();
         assertEquals(1, solutions.size());
         assertEquals(matrix, solutions.get(0));
     }
-    
+
     @Test
     public void testSolveWithOneStep() {
         // create full matrix
         GameMatrix matrix = Creator.createFull();
         Random random = new Random();
-        
+
         int row = random.nextInt(9);
         int column = random.nextInt(9);
-        
+
         Riddle riddle = new Riddle();
         riddle.setAll(matrix.getArray());
         riddle.set(row, column, GameMatrix.UNSET);
         riddle.setWritable(row, column, true);
-        
+
         Solver solver = new Solver(riddle);
         List<Riddle> solutions = solver.solve();
         assertEquals(1, solutions.size());
         assertEquals(matrix, solutions.get(0));
+    }
+
+    @Test
+    public void testSolveWithExampleProblem() {
+        byte[][] riddleBytes = GameMatrix.parse(
+                "......3..",
+                "9.3....2.",
+                ".....8.1.",
+                ".........",
+                "6...2..51",
+                "..8.5.79.",
+                "73.5..96.",
+                "5....2...",
+                "29.7.158.");
+        Riddle riddle = new Riddle();
+        riddle.setAll(riddleBytes);
+
+        byte[][] solutionBytes = GameMatrix.parse(
+                "862419375",
+                "913675428",
+                "475238619",
+                "159847236",
+                "647923851",
+                "328156794",
+                "731584962",
+                "586392147",
+                "294761583");
+        GameMatrix solution = new GameMatrix();
+        solution.setAll(solutionBytes);
+
+        Solver solver = new Solver(riddle);
+        List<Riddle> solutions = solver.solve();
+        assertEquals(1, solutions.size());
+        assertEquals(solution, solutions.get(0));
     }
 }
