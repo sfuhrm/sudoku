a295e7359d68cd6bef0d17b5b0b3004161d7781b
Stephan Fuhrmann <s@sfuhrm.de>
2018-04-26 18:43:53 +0200

    Breaking API change! Riddle instance creators probably want to use the GameMatrixFactory. Hiding of all GameMatrix impls behind interfaces Introduction of new factory class

---
+++ sudoku/src/main/java/de/sfuhrm/sudoku/GameMatrixFactory.java
@@ -0,0 +1,40 @@
+/*
+Sudoku - a fast Java Sudoku game creation library.
+Copyright (C) 2017-2018  Stephan Fuhrmann
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+Boston, MA  02110-1301, USA.
+ */
+package de.sfuhrm.sudoku;
+
+/**
+ * A factory for creating game matrix objects.
+ * @see GameMatrix
+ * @author Stephan Fuhrmann
+ */
+public final class GameMatrixFactory {
+
+    /** Creates a new game matrix factory instance. */
+    public GameMatrixFactory() {
+    }
+
+    /** Creates a new GameMatrix instance.
+     * @return a GameMatrix instance with all fields initialized
+     * to {@link GameMatrix#UNSET}.
+     */
+    public GameMatrix newGameMatrix() {
+        return new GameMatrixImpl();
+    }
+}
--- sudoku/src/main/java/de/sfuhrm/sudoku/Riddle.java
+++ sudoku/src/main/java/de/sfuhrm/sudoku/Riddle.java
@@ -1,49 +1,15 @@
 /*
-Sudoku - a fast Java Sudoku game creation library.
-Copyright (C) 2017-2018  Stephan Fuhrmann
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Library General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Library General Public License for more details.
-
-You should have received a copy of the GNU Library General Public
-License along with this library; if not, write to the
-Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
-Boston, MA  02110-1301, USA.
-*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package de.sfuhrm.sudoku;
 
 /**
- * The Sudoku game field.
- * This is a game matrix that has the information whether fields
- * may be written to or not.
+ * A riddle with free / writable fields.
  * @author Stephan Fuhrmann
  */
-public class Riddle extends CachedGameMatrixImpl implements Cloneable {
-
-    /**
-     * Whether the cell is writable. Pre-defined cells are only readable, use
-     * settable cells are writable.
-     */
-    private boolean[][] writeable;
-
-    /**
-     * Creates an empty full-writable riddle.
-     */
-    public Riddle() {
-        writeable = new boolean[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                setWritable(i, j, true);
-            }
-        }
-    }
+public interface Riddle extends GameMatrix {
 
     /**
      * Get whether a certain field is writable.
@@ -51,9 +17,7 @@ public class Riddle extends CachedGameMatrixImpl implements Cloneable {
      * @param column the column of the cell to get the writability for.
      * @return {@code true} if the cell is writable.
      */
-    public final boolean getWritable(final int row, final int column) {
-        return writeable[row][column];
-    }
+    boolean getWritable(final int row, final int column);
 
     /**
      * Set a certain field writable.
@@ -61,17 +25,6 @@ public class Riddle extends CachedGameMatrixImpl implements Cloneable {
      * @param column the column of the cell to set the writability for.
      * @param set the value to set for the cell, {@code true} means writable.
      */
-    public final void setWritable(final int row,
-            final int column,
-            final boolean set) {
-        writeable[row][column] = set;
-    }
+    void setWritable(final int row, final int column, final boolean set);
 
-    @Override
-    public final Object clone() {
-        Riddle clone;
-        clone = (Riddle) super.clone();
-        clone.writeable = QuadraticArrays.cloneArray(writeable);
-        return clone;
-    }
 }
